ecd4e1349ead5b18301277bd5d37002138224e8d
Wallas Levi <101941161+wallaslevi@users.noreply.github.com>
2022-03-20 17:20:52 -0300

    .

---
+++ exe1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+double argamassa=0.01;
+double alturatijolo=0;
+double comprimentotijolo=0;
+int quantidadetijolo=0;
+double alturaparede=0;
+double comprimentoparede=0;
+double areaparede=0;
+
+int main(){
+
+cout<<"ola\n vamos interagir um pouco?!\n\n";
+
+cout<<"informe a altura desejada da parede\n";
+cin>> alturaparede;
+
+cout<<"informe o comprimento desejado da parede\n";
+cin>>comprimentoparede;
+
+areaparede=alturaparede*comprimentoparede;
+
+cout<<"a area da parede e:" <<areaparede<<
+
+
+
+
+
+system("pause");
+return 0;
+
+}
