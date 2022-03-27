3153902b7582bafefd0a53bf3c17419dcf2639f5
Wallas Levi <101941161+wallaslevi@users.noreply.github.com>
2022-03-27 19:09:37 -0300

    ~

---
+++ exe3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+char opc,hm;
+
+int main(){
+
+
+cout<<setprecision(2)<<fixed;
+
+cout<<"\ncalculo de taxa de metabolismo basal TMB\n";
+cout<<"\nTMB e a quantidade de energia necessaria para a manutencao das funcoes vitais do organismo ao longo de 24 horas. ela e medida em calorias, que e a energia extraida pelo nosso corpo a partir dos macronutrientes (carboidratos, proteinas e gorduras totais)\n\n";
+
+inicio:
+
+cout<<"O calculo sera pra homem ou mulher?[H/M]";
+cin>>hm;
+
+if(hm=='h' or hm=='H'){
+
+
+
+}
+
+
+}
